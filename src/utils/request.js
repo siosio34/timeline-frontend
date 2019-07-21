b621391a369dab8c9632df4049bfb9a70ad57fd9
jo youngje <siosio34@nate.com>
2019-07-22 05:04:25 +0900

    [#52] Fix refresh token bug

--- src/utils/request.js
+++ src/utils/request.js
@@ -29,57 +29,38 @@ RequestApi.interceptors.request.use(
 
 const NOT_AUTHORIZED_HTTP_CODE = 401;
 
-let isRefreshing = false;
-const refreshSubscribers = [];
-
-function subscribeTokenRefresh(cb) {
-  refreshSubscribers.push(cb);
-}
-
-function onRrefreshed(token) {
-  refreshSubscribers.map(cb => cb(token));
-}
-
 RequestApi.interceptors.response.use(
   response => response.data,
   error => {
     const { config, response } = error;
     const originalRequest = config;
 
-    if (response && response.status === NOT_AUTHORIZED_HTTP_CODE) {
-      if (!isRefreshing) {
-        isRefreshing = true;
-        const refreshToken = window.localStorage.getItem('refresh_token');
-        return RequestApi.get(`/accounts/refresh/${refreshToken}`)
-          .then(({ data }) => {
-            const { access_token, refresh_token } = data;
-            window.localStorage.clear();
-            window.localStorage.setItem('access_token', access_token);
-            window.localStorage.setItem('refresh_token', refresh_token);
-            isRefreshing = false;
+    if (
+      response &&
+      response.status === NOT_AUTHORIZED_HTTP_CODE &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+      const refreshToken = window.localStorage.getItem('refresh_token');
+      return RequestApi.get(`/accounts/refresh/${refreshToken}`)
+        .then(data => {
+          const { access_token, refresh_token } = data;
 
-            onRrefreshed(access_token);
-          })
-          .catch(refreshTokenRequestError => {
-            if (
-              refreshTokenRequestError.response &&
-              refreshTokenRequestError.response.status ===
-                NOT_AUTHORIZED_HTTP_CODE
-            ) {
-              window.location.replace('/login');
-            }
-            return Promise.reject(error);
-          });
-      }
-
-      const retryOrigReq = new Promise(resolve => {
-        subscribeTokenRefresh(token => {
-          originalRequest.headers.Authorization = `bearer ${token}`;
-          resolve(RequestApi(originalRequest));
+          window.localStorage.setItem('access_token', access_token);
+          window.localStorage.setItem('refresh_token', refresh_token);
+          originalRequest.headers.Authorization = `bearer ${access_token}`;
+          return RequestApi(originalRequest);
+        })
+        .catch(refreshTokenRequestError => {
+          if (
+            refreshTokenRequestError.response &&
+            refreshTokenRequestError.response.status ===
+              NOT_AUTHORIZED_HTTP_CODE
+          ) {
+            window.location.replace('/login');
+          }
+          return Promise.reject(error);
         });
-      });
-
-      return retryOrigReq;
     }
     return Promise.reject(error);
   },
