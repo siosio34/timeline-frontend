197ab80e0aa2a2c9cd6ff0ce7c21d63733225d47
cutelee <soyoung.dev@gmail.com>
2019-07-21 13:26:38 +0900

    [#27] Edit-signup-link
    * 회원가입 링크 주소 수정 (join > register)
    * 회원가입 링크 마크업 수정 : form 바깥으로 분리

--- src/pages/signin/SignInPage.js
+++ src/pages/signin/SignInPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import SigninForm from './signinForm';
+import './SignInPage.css';
 
 class SignInPage extends React.Component {
   componentDidMount() {}
@@ -10,6 +11,9 @@ class SignInPage extends React.Component {
       <div style={{ padding: '50px', maxWidth: '500px', margin: 'auto' }}>
         <h1 style={{textAlign: 'center'}}>로그인</h1>
         <SigninForm />
+        <div className="register-link">
+          아직 회원이 아니라면 <Link to="/register">회원가입</Link>해주세요.
+        </div>
       </div>
     );
   }
