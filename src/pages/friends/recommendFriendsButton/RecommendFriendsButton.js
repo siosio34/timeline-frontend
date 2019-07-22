c4cf7d84b45092d14884dd642fddf4502300cf86
cutelee <soyoung.dev@gmail.com>
2019-07-22 10:58:06 +0900

    [#58] Fix-recommend-list-button
    * 추천 친구목록 버튼 상태 구분하는 boolean 변수 수정

--- src/pages/friends/recommendFriendsButton/RecommendFriendsButton.js
+++ src/pages/friends/recommendFriendsButton/RecommendFriendsButton.js
@@ -2,21 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'antd';
 
-const RecommendFriendsButton = ({ friendInfo = {} }) => {
-  const { requested } = friendInfo;
+const RecommendFriendsButton = ({ friend }) => {
+  const { relationship } = friend;
+  const isRequested = relationship === 'REQUEST';
+
+  if (!isRequested) {
+    return (
+      <Button size="small">
+        친구신청
+      </Button>
+    );
+  }
+
   return (
-    <Button size="small" type={requested ? 'RequestFriendsButton' : 'dashed'}>
-      {requested ? '친구신청' : '요청 중'}
+    <Button size="small" type='dashed'>
+      요청 중
     </Button>
   );
 };
 
 RecommendFriendsButton.defaultProps = {
-  friendInfo: {},
+  friend: {},
 };
 
 RecommendFriendsButton.propTypes = {
-  friendInfo: PropTypes.object,
+  friend: PropTypes.object,
 };
 
 export default RecommendFriendsButton;
