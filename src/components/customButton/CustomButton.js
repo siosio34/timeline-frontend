91e7df43c558a72451addab1b0a99f8698820c38
cutelee <soyoung.dev@gmail.com>
2019-07-30 18:59:45 +0900

    [#156] 필요없는 컴포넌트 제거

--- src/components/customButton/CustomButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Button } from 'antd';
-
-const CustomButton = ({ ...props }) => (
-  <Button {...props} type="primary">
-    예시
-  </Button>
-);
-export default CustomButton;
